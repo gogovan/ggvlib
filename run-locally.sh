d737d753096d40bfe3e0dc0e949aa905f1f20156
Adam Hoffstein <adam@C0377-Macbook-Pro.local>
2022-10-10 20:58:28 +0800

    hotfix: added env var to run-locally.sh

--- run-locally.sh
+++ run-locally.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export GOOGLE_APPLICATION_CREDENTIALS=bq-service-account.json
 export BUCKET=ggx-analytics
+export COUNTRIES="hk|vn|sg"
 poetry shell
 poetry run python local.py
